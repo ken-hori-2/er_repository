4e4d783b99db9aae05d69496fdc2b6fc25df83de
ken-hori-2 <stmuymte@gmail.com>
2024-05-10 00:13:28 +0900

    202405

--- create_files.sh
+++ create_files.sh
@@ -5,7 +5,7 @@ mkdir -p 202405/{plan,result}
 for day in {01..31}
 do
   if [ $day -lt 10 ]; then
-    touch 202405/2024_05_0$day.txt
+    touch 202405/2024_05_$day.txt
   else
     touch 202405/2024_05_$day.txt
   fi
